f0cec0e0553d69c6bb3268807aaa1774f319db73
haticesezgin <131539164+haticesezgin@users.noreply.github.com>
2023-06-08 18:08:40 +0300

    post.blade.php

---
+++ resources/views/front/website/post.blade.php
@@ -0,0 +1,78 @@
+
+@extends('front.website.master')
+@section('title')
+    post
+@endsection
+@section('content')
+    <div class="site-cover site-cover-sm same-height overlay single-page" style="background-image: url('13C1C34E-D978-4713-9FC7-6FADE5177541.png');">
+        <div class="container">
+            <div class="row same-height justify-content-center">
+                <div class="col-md-12 col-lg-10">
+                    <div class="post-entry text-center">
+                        <span class="post-category text-white bg-success mb-3">Bilim Kurgu</span>
+                        <h1 class="mb-4"><a href="#">Otostopçunun Galaksi Rehberi – Douglas Adams</a></h1>
+                        <div class="post-meta align-items-center text-center">
+                            <figure class="author-figure mb-0 mr-3 d-inline-block"><img src="Hannibal-Cinerituellllllll.png" alt="Image" class="img-fluid"></figure>
+                            <span class="d-inline-block mt-1">Hatice Sezgin</span>
+                            <span>&nbsp;6 Nisan 2023</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="site-section py-lg">
+        <div class="container">
+
+            <div class="row blog-entries element-animate">
+
+                <div class="col-md-12 col-lg-8 main-content">
+
+                    <div class="post-content-body">
+                        <p class="text font-italic">Otostopçunun Galaksi Rehberi‘ni</p><p>ilk olarak nereden duyduğumu bir türlü hatırlayamıyorum ama bir şekilde okumak istediğim kitaplar arasına girmişti. Douglas Adams‘a ait bu kitabın kült bir eser olduğunu biliyordum, seveninin çok olduğunu da. Kitaplarda ya da filmlerde de yine karşıma çıkıyordu. O halde okuma zamanı gelmişti artık.Bu beş ciltli kitabı çoğu yerde Alfa Yayınları’na ait versiyonunu bulabilirsiniz.
+                            Otostopçunun Galaksi Rehberi, ilk başlarda gözümde bir gezi kitabı gibi canlanmıştı, nedense!
+                           Arthur Dent, basit bir yaşam süren bir dünyalı. Ford Prefect ise yıllar önce dünyada mahsur kalmış, başka gezegenden bir otostopçu. Daha doğrusu, Otostopçunun Galaksi Rehberi adlı rehber kitap için bilgi toplarken yolu dünyaya düşmüş. Bir gün, dünyanın yok edileceği bilgisini alan Ford, arkadaşı Arthur’u bu saldırıdan son dakika kurtarır, ve kendilerini bir uzay gemisinde bulurlar. Böylece Arthur’a oldukça yabancı gelen bir evrendeki maceraları başlamış olur.</a>
+                           Bu macera sırasında Arthur dünyanın asıl yaratılış nedenini öğrenecek, biz dünyalılar için önemli saydığımız birçok şeyin aslında önemsiz detaylar olduğunu, biz insanların birlikte yaşadığımız, hatta sevmediğimiz bazı canlılar için deney unsurları olduğumuzu öğrenecektir. Bu bilgiler Arthur’u oldukça şaşırtırken ilginç bir şekilde kendini bir anda evrenin nihai sorusunu ararken bulacaktır. İlginç bir şekilde diyorum, çünkü aslında cevabı belli olan ancak sorusu belli olmayan bu sorudan bahsediyoruz.
+
+                            Otostopçunun Galaksi Rehberi, itiraf ediyorum başlarda basit bir uzay romanı olarak gözüktü bana. Bildiğimiz, Star Wars- Star Trek gibi uzay filmleri veya romanlarından önce bu kitap olduğu için herhalde zamanında beğenilmiş diyordum. Ancak kitap beni yanılttı. Kitabı elimden bırakamadım. İçerdiği espriler, konunun size anlatılış biçimi, eski romanlarda gördüğümüz olaya müdahale ederek aslında karakterler böyle sanıyor ama olayın aslı bu şekilde şeklinde açıklamalarla kitap sizi sarıyor</p>
+
+                        <div class="row mb-5 mt-5">
+                            <div class="col-md-12 mb-4">
+                                <img src="{{asset('website')}}/images/img_1.jpg" alt="Image placeholder" class="img-fluid rounded">
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <img src="{{asset('website')}}/images/img_2.jpg" alt="Image placeholder" class="img-fluid rounded">
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <img src="{{asset('website')}}/images/img_3.jpg" alt="Image placeholder" class="img-fluid rounded">
+                            </div>
+                        </div>
+                        <p>Otostopçunun Galaksi Rehberi, 5 kitaplık bir serinin ilk kitabı. Kitabın diğer seri kitaplardan da bir farkı var. Genelde her kitapta bir genel olay olur, çözümlenir, kitabın sonunda yeni bir problem çıkar ve diğer kitap buna odaklanmış olur. Ancak Bu kitapta baştan beri cevaplanmayan bir sürü sorular var, çözülmemiş olaylar var. Kitapta genel olarak şöyle bir olay oldu ve sonunda çözümlendi diyebileceğimiz bir durum yok. Olaylar ardı ardına gerçekleşiyor, belki de sorun 5.’in kitabın sonunda ancak çözülecek izlenimi verdi bana.
+
+                            Kısacası, Otostopçunun Galaksi Rehberi, Douglas Adams tarafından yazılan aslında bir seri. Benim gibi yanılıp sadece ilk kitabını okurum demeyin. Çünkü kesinlikle tamamlanmamış, giriş niteliğinde bir kitap. Bizi diğer kitaplarda çok fazla şeyin beklediğine eminim.
+
+                            Serinin tüm kitapları sırasıyla şu şekilde;
+
+                            Otostopçunun Galaksi Rehberi
+
+                            Evrenin Sonundaki Restoran
+
+                            Hayat, Evren ve Her şey
+
+                            Elveda ve Bütün O Balıklar İçin Teşekkürler
+
+                            Çoğunlukla Zararsız</p>
+
+                    </div>
+
+                    <div class="pt-5">
+                        <p>Kategoriler:  <a href="#">Mizah</a>, <a href="#">Bilim kurgu</a>  Tags: <a href="#">#Mizahi</a>, <a href="#">#Ütopik</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
+
+

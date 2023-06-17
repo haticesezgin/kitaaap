46fe04714089c0cf9a72c1c212489d3d4f38d78b
haticesezgin <131539164+haticesezgin@users.noreply.github.com>
2023-06-17 20:19:33 +0300

    post.blade.php

--- resources/views/front/website/post.blade.php
+++ resources/views/front/website/post.blade.php
@@ -31,8 +31,8 @@
                     <div class="post-content-body">
                         <p class="text font-italic">Otostopçunun Galaksi Rehberi‘ni</p><p>ilk olarak nereden duyduğumu bir türlü hatırlayamıyorum ama bir şekilde okumak istediğim kitaplar arasına girmişti. Douglas Adams‘a ait bu kitabın kült bir eser olduğunu biliyordum, seveninin çok olduğunu da. Kitaplarda ya da filmlerde de yine karşıma çıkıyordu. O halde okuma zamanı gelmişti artık.Bu beş ciltli kitabı çoğu yerde Alfa Yayınları’na ait versiyonunu bulabilirsiniz.
                             Otostopçunun Galaksi Rehberi, ilk başlarda gözümde bir gezi kitabı gibi canlanmıştı, nedense!
-                           Arthur Dent, basit bir yaşam süren bir dünyalı. Ford Prefect ise yıllar önce dünyada mahsur kalmış, başka gezegenden bir otostopçu. Daha doğrusu, Otostopçunun Galaksi Rehberi adlı rehber kitap için bilgi toplarken yolu dünyaya düşmüş. Bir gün, dünyanın yok edileceği bilgisini alan Ford, arkadaşı Arthur’u bu saldırıdan son dakika kurtarır, ve kendilerini bir uzay gemisinde bulurlar. Böylece Arthur’a oldukça yabancı gelen bir evrendeki maceraları başlamış olur.</a>
-                           Bu macera sırasında Arthur dünyanın asıl yaratılış nedenini öğrenecek, biz dünyalılar için önemli saydığımız birçok şeyin aslında önemsiz detaylar olduğunu, biz insanların birlikte yaşadığımız, hatta sevmediğimiz bazı canlılar için deney unsurları olduğumuzu öğrenecektir. Bu bilgiler Arthur’u oldukça şaşırtırken ilginç bir şekilde kendini bir anda evrenin nihai sorusunu ararken bulacaktır. İlginç bir şekilde diyorum, çünkü aslında cevabı belli olan ancak sorusu belli olmayan bu sorudan bahsediyoruz.
+                            Arthur Dent, basit bir yaşam süren bir dünyalı. Ford Prefect ise yıllar önce dünyada mahsur kalmış, başka gezegenden bir otostopçu. Daha doğrusu, Otostopçunun Galaksi Rehberi adlı rehber kitap için bilgi toplarken yolu dünyaya düşmüş. Bir gün, dünyanın yok edileceği bilgisini alan Ford, arkadaşı Arthur’u bu saldırıdan son dakika kurtarır, ve kendilerini bir uzay gemisinde bulurlar. Böylece Arthur’a oldukça yabancı gelen bir evrendeki maceraları başlamış olur.</a>
+                            Bu macera sırasında Arthur dünyanın asıl yaratılış nedenini öğrenecek, biz dünyalılar için önemli saydığımız birçok şeyin aslında önemsiz detaylar olduğunu, biz insanların birlikte yaşadığımız, hatta sevmediğimiz bazı canlılar için deney unsurları olduğumuzu öğrenecektir. Bu bilgiler Arthur’u oldukça şaşırtırken ilginç bir şekilde kendini bir anda evrenin nihai sorusunu ararken bulacaktır. İlginç bir şekilde diyorum, çünkü aslında cevabı belli olan ancak sorusu belli olmayan bu sorudan bahsediyoruz.
 
                             Otostopçunun Galaksi Rehberi, itiraf ediyorum başlarda basit bir uzay romanı olarak gözüktü bana. Bildiğimiz, Star Wars- Star Trek gibi uzay filmleri veya romanlarından önce bu kitap olduğu için herhalde zamanında beğenilmiş diyordum. Ancak kitap beni yanılttı. Kitabı elimden bırakamadım. İçerdiği espriler, konunun size anlatılış biçimi, eski romanlarda gördüğümüz olaya müdahale ederek aslında karakterler böyle sanıyor ama olayın aslı bu şekilde şeklinde açıklamalarla kitap sizi sarıyor</p>
 
@@ -67,12 +67,98 @@
 
                     <div class="pt-5">
                         <p>Kategoriler:  <a href="#">Mizah</a>, <a href="#">Bilim kurgu</a>  Tags: <a href="#">#Mizahi</a>, <a href="#">#Ütopik</a></p>
-                    </div>
+                        <div class="pt-5">
+                            <h3 class="mb-5">6 Yorumlar</h3>
+                            <ul class="comment-list">
+                                <li class="comment">
+                                    <div class="vcard">
+                                        <img src="Hannibal-Cinerituellllllll.png" alt="Image placeholder">
+                                    </div>
+                                    <div class="comment-body">
+                                        <h3>Hatice Sezgin</h3>
+                                        <div class="meta">6 nisan 2023 ,15:11</div>
+                                        <p>Yorum Kısmı</p>
+                                        <p><a href="#" class="reply rounded">Cevapla</a></p>
+                                    </div>
+                                </li>
+
+                                <li class="comment">
+                                    <div class="vcard">
+                                        <img src="Hannibal-Cinerituellllllll.png" alt="Image placeholder">
+                                    </div>
+                                    <div class="comment-body">
+                                        <h3>Hatice sezgin</h3>
+                                        <div class="meta">6 nisan 2023 ,15:11</div>
+                                        <p>Yorum Kısmı</p>
+                                        <p><a href="#" class="reply rounded">Cevapla</a></p>
+                                    </div>
+
+                                    <ul class="children">
+                                        <li class="comment">
+                                            <div class="vcard">
+                                                <img src="Hannibal-Cinerituellllllll.png" alt="Image placeholder">
+                                            </div>
+                                            <div class="comment-body">
+                                                <h3>Hatice sezgin</h3>
+                                                <div class="meta">6 nisan 2023, 15:11</div>
+                                                <p>Yorum Kısmı</p>
+                                                <p><a href="#" class="reply rounded">Cevapla</a></p>
+                                            </div>
+
+
+                                            <ul class="children">
+                                                <li class="comment">
+                                                    <div class="vcard">
+                                                        <img src="Hannibal-Cinerituellllllll.png" alt="Image placeholder">
+                                                    </div>
+                                                    <div class="comment-body">
+                                                        <h3>Hatice Sezgin</h3>
+                                                        <div class="meta">6 nisan 2023, 15:11</div>
+                                                        <p>Yorum Kısmı</p>
+                                                        <p><a href="#" class="reply rounded">Cevapla</a></p>
+                                                    </div>
+
+                                                    <ul class="children">
+                                                        <li class="comment">
+                                                            <div class="vcard">
+                                                                <img src="Hannibal-Cinerituellllllll.png" alt="Image placeholder">
+                                                            </div>
+                                                            <div class="comment-body">
+                                                                <h3>Hatice sezgin</h3>
+                                                                <div class="meta">6 nisan 2023, 15:11</div>
+                                                                <p>Yorum Kısmı</p>
+                                                                <p><a href="#" class="reply rounded">Cevapla</a></p>
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                <li class="comment">
+                                    <div class="vcard">
+                                        <img src="Hannibal-Cinerituellllllll.png" alt="Image placeholder">
+                                    </div>
+                                    <div class="comment-body">
+                                        <h3>Hatice sezgin</h3>
+                                        <div class="meta">6 nisan 2023 15:11</div>
+                                        <p>Yorum Kısmı</p>
+                                        <p><a href="#" class="reply rounded">Cevapla</a></p>
+                                    </div>
+                                </li>
+                            </ul>
+
+
+                        </div>
                 </div>
             </div>
+        </div>
         </div>
     </section>
 
 @endsection
+
 
 
